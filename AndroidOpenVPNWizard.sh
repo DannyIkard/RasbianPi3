3af1fcebf23e72b266b6b217bf79012ff5c10fdf
Danny Ikard <danielikard@gmail.com>
2017-05-03 08:02:42 +0000

    Started AndroidOpenVPNWizard.sh

---
+++ AndroidOpenVPNWizard.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "==== Android OpenVPN Wizard ====
+echo "-- Installing OpenVPN - 'apt-get update'"
+sudo apt-get update
+echo "-- Installing OpenVPN - 'apt-get install openvpn'"
+sudo apt-get install openvpn
